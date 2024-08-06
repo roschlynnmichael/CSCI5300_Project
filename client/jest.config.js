45ada760485a7e03879cb0394935b93739d0ffd7
Roschlynn Michael Dsouza <roschlynn@outlook.com>
2024-08-06 10:30:37 -0500

    Updated babel config and jest config to try and pass unit tests

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,13 +1,12 @@
 module.exports = {
-    transform: {
+  transform: {
       "^.+\\.jsx?$": "babel-jest",
-    },
-    transformIgnorePatterns: [
-      "/node_modules/(?!(axios)/)"
-    ],
-    moduleFileExtensions: ["js", "jsx"],
-    testMatch: [
+  },
+  transformIgnorePatterns: [
+      "/node_modules/(?!(axios|@testing-library)/)" // Allow transformation of @testing-library packages
+  ],
+  moduleFileExtensions: ["js", "jsx"],
+  testMatch: [
       "<rootDir>/src/__tests__/**/*.(test|spec).js"
-    ],
-  };
-  
+  ],
+};
