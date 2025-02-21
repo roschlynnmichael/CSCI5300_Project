af6db34a9bae5998683f808407e269c25e8488a6
Abhilash Kurapati <abhilash.kurapati@slu.edu>
2025-02-20 23:10:47 -0600

    Deployement trial-3

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -6,7 +6,7 @@ const authMiddleware = require('../middleware/authMiddleware');
 const userController=require('../controllers/userController');
 
 router.use(cors({ // Enable CORS for all routes
-    origin: 'http://localhost:3000',
+    origin: 'https://easy-budget-4zd6.onrender.com/',
 }));
 
 router.post('/register', userController.register);
