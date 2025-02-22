bf9538613de48bf4ae8b3cc8f19b458bff8d503b
Abhilash Kurapati <abhilash.kurapati@slu.edu>
2025-02-21 23:34:40 -0600

    fall back from deployment

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -6,8 +6,7 @@ const authMiddleware = require('../middleware/authMiddleware');
 const userController=require('../controllers/userController');
 
 router.use(cors({ // Enable CORS for all routes
-    origin: ['https://easy-budget-4zd6.onrender.com','http://localhost:3000'],
-    credentials : true
+    origin: 'https://easy-budget-4zd6.onrender.com',
 }));
 
 router.post('/register', userController.register);
